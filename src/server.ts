e683669144f9679da65450f2522130f635b78e2a
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-02-10 11:01:27 -0300

    feat: config vercel!

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,16 @@ import { app } from "./app";
 // import { connectToMongoDb } from "./database/mongo";
 import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "./utils/config/swagger.json";
+import { Request, Response } from "express";
 
 const PORT = process.env.PORT || 5000;
 
 const createServer = async () => {
   // await connectToMongoDb();
   app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+  app.get('/', (_req: Request, res: Response) => {
+    return res.send('Express Typescript on Vercel')
+  })
   app.listen(PORT, () => {
     console.log(`Running at http://localhost:${PORT}`);
     console.log(`Documentation at http://localhost:${PORT}/api/docs`);
