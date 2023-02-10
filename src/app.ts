526fccb1a23c51200e138396cb93b7d21b960190
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-02-10 10:32:02 -0300

    feat: configure server and app

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+// import { router } from "./routes";
+
+import cors from "cors";
+// import { connectToMongoDb } from "./database/mongo";
+
+// connectToMongoDb();
+
+const app = express();
+
+const corsOptions = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(express.json());
+// app.use(router);
+
+export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log("teste, olá mundo")
+import * as dotenv from "dotenv";
+dotenv.config();
+import { app } from "./app";
+// import { connectToMongoDb } from "./database/mongo";
+import swaggerUI from "swagger-ui-express";
+import swaggerDocument from "./utils/config/swagger.json";
+
+const PORT = process.env.PORT || 5000;
+
+const createServer = async () => {
+  // await connectToMongoDb();
+  app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+  app.listen(PORT, () => {
+    console.log(`Running at http://localhost:${PORT}`);
+    console.log(`Documentation at http://localhost:${PORT}/api/docs`);
+  });
+};
+createServer();
