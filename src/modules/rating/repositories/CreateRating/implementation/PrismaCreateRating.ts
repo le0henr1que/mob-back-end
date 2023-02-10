1d68ca5daf1b342866163f1407e63a2c28ab1d26
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-02-10 16:58:44 -0300

    feat: module create rating

--- src/modules/rating/repositories/CreateRating/implementation/PrismaCreateRating.ts
+++ src/modules/rating/repositories/CreateRating/implementation/PrismaCreateRating.ts
@@ -8,7 +8,6 @@ export class PrismaRepositoryCreateRating implements ICreateRating  {
     async executeCreate(dataRating:Rating): Promise<Rating> {
         const prisma = new PrismaClient()
         const {score, localId, userId} = dataRating
-
         return await prisma.rating.create({data:{
             userId: userId,
             localId: localId,
