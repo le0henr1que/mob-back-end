bf0d42cfdac24209bd3d88a36232f9fe8051f323
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-02-12 11:52:28 -0300

    feat: create interface delete Rating

---
+++ src/modules/rating/repositories/DeleteRating/implementation/PrismaDeleteRating.ts
@@ -0,0 +1,16 @@
+import { Rating } from "types";
+import { IDeleteRating } from "../IDeleteRating"
+import { PrismaClient } from "@prisma/client"
+
+export class PrismaRepositoryDeleteRating implements IDeleteRating {
+    async executeDelete(ratingId:string): Promise<void> {
+        const prisma = new PrismaClient();
+
+        await prisma.rating.delete({
+            where:{
+                id:ratingId
+            }
+        })
+
+    }
+}
