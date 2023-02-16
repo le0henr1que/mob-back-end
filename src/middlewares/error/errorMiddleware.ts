834384df6a89ccb08457a29ccc0292c9ba9003e7
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-02-15 22:22:46 -0300

    feat: middleware error

--- src/middlewares/error/errorMiddleware.ts
+++ src/middlewares/error/errorMiddleware.ts
@@ -9,16 +9,16 @@ export function errorMiddleware(err: Error, req: Request, res: Response, next: N
         
     console.log("Middleware pass instance of")
 
-//     return res.status(err.statusCode).json({
-//       status: 'error',
-//       message: err.message,
-//     });
+    return res.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
     
   }
 
-//   return res.status(500).json({
-//     status: 'error',
-//     message: 'Internal server error',
-//   });
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
   
 }
