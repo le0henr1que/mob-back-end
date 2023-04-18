bf8882d126a7828b4da244bc9476ef8a5d32fa29
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-04-18 15:09:26 -0300

    feat:resolve list rating

--- src/modules/local/repositories/RatingListByLocal/implementation/PrismaRatingListByLocal.ts
+++ src/modules/local/repositories/RatingListByLocal/implementation/PrismaRatingListByLocal.ts
@@ -3,12 +3,28 @@ import { RatingListByLocal } from '../RatingListByLocal';
 import { PrismaClient } from '@prisma/client';
 
 export class PrismaRepositoryRatingListByLocal implements RatingListByLocal {
-  async executeRatingListByLocal(localId: string): Promise<Rating[]> {
+  async executeRatingListByLocal(localId: string): Promise<any> {
     const prisma = new PrismaClient();
 
     return await prisma.rating.findMany({
       where: {
         localId: localId,
+      },
+      select: {
+        id: true,
+        userId: false,
+        rating: true,
+        user: {
+          select: {
+            id: true,
+            password: false,
+            name: true,
+            email: true,
+            accepted_terms: false,
+          },
+        },
+        localId: false,
+        comment: true,
       },
     });
   }
