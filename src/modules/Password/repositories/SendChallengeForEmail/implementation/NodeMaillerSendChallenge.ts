c561aa83e5184e0394d6d5e410e2cdabb20ecec4
le0henr1que <leonardoferreira.henrique1210@gmail.com>
2023-05-09 16:04:39 -0300

    refactor: Separating email sending responsibility in a module

--- src/modules/Password/repositories/SendChallengeForEmail/implementation/NodeMaillerSendChallenge.ts
+++ src/modules/Password/repositories/SendChallengeForEmail/implementation/NodeMaillerSendChallenge.ts
@@ -2,14 +2,27 @@ import nodemailer from 'nodemailer';
 import { EmailService } from '../ISendChallengeForEmail';
 import transporter from '../../../../../config/Email/emailConfig';
 import { PrismaClient } from '@prisma/client';
+import fs from 'fs';
+import handlebars from 'handlebars';
 
 export class NodemailerEmailService implements EmailService {
   async sendEmail(to: string, codeChallenge: string): Promise<void> {
+    const templateHtml = fs.readFileSync('src/Views/Template/ResetPassword/index.html', 'utf-8');
+    const compiledTemplate = handlebars.compile(templateHtml);
+
+    const data = {
+      title: 'Alteração de Senha',
+      name: 'John Doe',
+      code: codeChallenge,
+    };
+
+    const html = compiledTemplate(data);
+
     const mailOptions = {
       from: 'no-reply@mob.com',
       to,
-      subject: 'Alteração de senha',
-      text: `Alteração de senha, seu código é: ${codeChallenge}`,
+      subject: 'aqui está seu código',
+      html: html,
     };
 
     await transporter.sendMail(mailOptions);
